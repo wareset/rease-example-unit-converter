1d019bc9a1dadb9820ba3bcd27e12ed19b8768cb
rease <cftcview@gmail.com>
2022-08-19 11:11:44 +0500

    remove old caches

--- src/__index.rease.client.tsx
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-import { createReaseApp } from 'rease'
-
-import { App } from './App.rease'
-
-createReaseApp(App, { target: document.body })
